6067bfb9948f5f0f6a88b6e95894bbf53672f679
Andrew Spaulding <andyespaulding@gmail.com>
2023-01-10 20:33:21 -0500

    Rolled back RVAScan library to original version

--- reg2k/RVA.h
+++ reg2k/RVA.h
@@ -2,15 +2,15 @@
 #include <unordered_map>
 #include <memory>
 
-#include "skse_version.h"
+#include "skse64_common/skse_version.h"
 #include "Relocation.h"
 
 #include "Pattern.h"
 
 //=============================================================================================
-//=======================================    RVAScan.h    =====================================
+//=======================================    RVAScan.h    =========================================
 //=============================================================================================
-//====      An RVAScan encapsulates a version-specific relative virtual address and a      ====
+//====      An RVAScan encapsulates a version-specific relative virtual address and a          ====
 //====                 version-independent address signature pattern.                      ====
 //=============================================================================================
 //====      If addresses are explicitly specified for the current runtime version,         ====
@@ -23,8 +23,6 @@
 //=============================================================================================
 //====      Author: registrator2000                                                        ====
 //=============================================================================================
-
-// Me was here. Vandalized everything. No more runtimeVersion only signatures.
 
 #define SHOW_ADDR                    2
 #define GET_RVA(relocPtr) relocPtr.GetUIntPtr() - RelocationManager::s_baseAddr
@@ -46,7 +44,7 @@ namespace RVAUtils
 
 struct RVAData
 {
-    const char* name;
+    std::unordered_map<UInt32, uintptr_t>    addr;   // Map of runtime version to RVAScan.
     const char*                                sig = nullptr;     // Signature
     uintptr_t                                effectiveAddress = 0;
     int                                        offset = 0;
@@ -86,37 +84,55 @@ namespace RVAUtils
 class RVAManager
 {
 public:
-    static void UpdateSingle(std::shared_ptr<RVAData> rvaData) {
-        // Sigscan
-        if (rvaData->sig)
+    static void UpdateAddresses(UInt32 runtimeVersion) {
+        RVAUtils::Timer tmr;
+
+        for (auto rvaData : m_rvaDataVec())
         {
-            rvaData->effectiveAddress = (uintptr_t)Utility::pattern(rvaData->sig).count(1).get(0).get<void>(rvaData->offset);
-
-            if (rvaData->indirectOffset != 0)
-            {
-                SInt32 rel32 = 0;
-                RVAUtils::ReadMemory(rvaData->effectiveAddress + rvaData->indirectOffset, &rel32, sizeof(SInt32));
-                rvaData->effectiveAddress = rvaData->effectiveAddress + rvaData->instructionLength + rel32;
-            }
-
-#if SHOW_ADDR
-            _MESSAGE("---");
-            _MESSAGE("name: %s sig: %s", rvaData->name, rvaData->sig);
-            _MESSAGE("effective address: %p", rvaData->effectiveAddress);
-            _MESSAGE("RVAScan: 0x%08X", rvaData->effectiveAddress - RelocationManager::s_baseAddr);
-            _MESSAGE("---");
-#endif
-
-        }
-        else
-        {
-            _MESSAGE("Warning: No signature and no addresses for runtime.");
+            if (rvaData->effectiveAddress) continue;
+            UpdateSingle(rvaData, runtimeVersion);
         }
     }
 
-    /*static uintptr_t GetEffectiveAddress(uintptr_t rva) {
+    static void UpdateSingle(std::shared_ptr<RVAData> rvaData, UInt32 runtimeVersion = 0) {
+        if (SHOW_ADDR != 1 && rvaData->addr.count(runtimeVersion) > 0) {
+            rvaData->effectiveAddress = GetEffectiveAddress(rvaData->addr[runtimeVersion]);
+        }
+        else {
+            // Sigscan
+            if (rvaData->sig)
+            {
+                rvaData->effectiveAddress = (uintptr_t)Utility::pattern(rvaData->sig).count(1).get(0).get<void>(rvaData->offset);
+
+                if (rvaData->indirectOffset != 0)
+                {
+                    SInt32 rel32 = 0;
+                    RVAUtils::ReadMemory(rvaData->effectiveAddress + rvaData->indirectOffset, &rel32, sizeof(SInt32));
+                    rvaData->effectiveAddress = rvaData->effectiveAddress + rvaData->instructionLength + rel32;
+                }
+
+#if SHOW_ADDR
+                _MESSAGE("---");
+                _MESSAGE("sig: %s", rvaData->sig);
+                _MESSAGE("effective address: %p", rvaData->effectiveAddress);
+                _MESSAGE("RVAScan: 0x%08X", rvaData->effectiveAddress - RelocationManager::s_baseAddr);
+                _MESSAGE("---");
+#endif
+
+            }
+            else
+            {
+                _MESSAGE("Warning: No signature and no addresses for runtime.");
+                for (auto addr : rvaData->addr) {
+                    _MESSAGE("Runtime version: %08X \tAddress: %08X", addr.first, addr.second);
+                }
+            }
+        }
+    }
+
+    static uintptr_t GetEffectiveAddress(uintptr_t rva) {
         return RelocationManager::s_baseAddr + rva;
-    }*/
+    }
 
     static void Add(std::shared_ptr<RVAData> data) {
         m_rvaDataVec().push_back(data);
@@ -135,8 +151,42 @@ template <typename T = void>
 class RVAScan
 {
 public:
-    RVAScan(const char* name, const char* sig, int offset = 0, int indirectOffset = 0, int instructionLength = 0) {
-        init(name, sig, offset, indirectOffset, instructionLength);
+    using AddressMap = std::unordered_map<UInt32, uintptr_t>;
+
+    RVAScan(AddressMap addr, const char* sig, int offset = 0, int indirectOffset = 0, int instructionLength = 0) {
+        init(addr, sig, offset, indirectOffset, instructionLength);
+    }
+
+    RVAScan(const char* sig, int offset, AddressMap addr) {
+        init(addr, sig, offset);
+    }
+
+    RVAScan(const char* sig, AddressMap addr)
+    {
+        init(addr, sig, 0);
+    }
+
+    // Address map only
+    RVAScan(AddressMap addr) {
+        init(addr, NULL, 0);
+    }
+
+    // Address only
+    RVAScan(uintptr_t rva) {
+        AddressMap addr = { { CURRENT_RELEASE_RUNTIME, rva } };
+        init(addr, NULL, 0);
+    }
+
+    // Address + sig
+    RVAScan(uintptr_t rva, const char* sig, int offset = 0, int indirectOffset = 0, int instructionLength = 0) {
+        AddressMap addr/* = { { CURRENT_RELEASE_RUNTIME, rva } }*/;
+        init(addr, sig, offset, indirectOffset, instructionLength);
+    }
+
+    // Signature only
+    RVAScan(const char* sig, int offset = 0, int indirectOffset = 0, int instructionLength = 0) {
+        AddressMap addr;
+        init(addr, sig, offset, indirectOffset, instructionLength);
     }
 
     // Default constructor (empty)
@@ -179,27 +229,34 @@ public:
         return (data->effectiveAddress != NULL);
     }
 
-    void Resolve() {
-        if (!IsResolved()) RVAManager::UpdateSingle(data);
+    void Resolve(UInt32 runtimeVersion) {
+        if (!IsResolved()) RVAManager::UpdateSingle(data, runtimeVersion);
     }
 
-    /*void SetEffective(uintptr_t ea) {
+    void Set(uintptr_t rva) {
+        data->effectiveAddress = RVAManager::GetEffectiveAddress(rva);
+    }
+
+    void SetEffective(uintptr_t ea) {
         data->effectiveAddress = ea;
-    }*/
+    }
+
+    //void operator=(RVAScan const&) = delete;
 
 private:
     std::shared_ptr<RVAData> data;
 
-    void init(const char* name, const char* sig, int offset, int indirectOffset = 0, int instructionLength = 0)
+    void init(AddressMap addr, const char* sig, int offset, int indirectOffset = 0, int instructionLength = 0)
     {
         data = std::make_shared<RVAData>();
-        data->name = name;
+        data->addr = addr;
         data->sig = sig;
         data->offset = offset;
         data->indirectOffset = indirectOffset;
         data->instructionLength = instructionLength;
 
-        RVAManager::UpdateSingle(data);
+        RVAManager::UpdateSingle(data, CURRENT_RELEASE_RUNTIME);
+        //RVAManager::Add(data);
     }
 };
 
